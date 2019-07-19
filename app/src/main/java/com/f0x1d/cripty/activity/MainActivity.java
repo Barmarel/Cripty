c2efa55b313b6320e549c55aab9c19d733acab15
F0x1d <maks.zrandles@gmail.com>
2019-07-19 22:56:48 +0500

    Initial commit

---
+++ app/src/main/java/com/f0x1d/cripty/activity/MainActivity.java
@@ -0,0 +1,34 @@
+package com.f0x1d.cripty.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import com.f0x1d.cripty.R;
+import com.f0x1d.cripty.fragment.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 228);
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(Color.GRAY);
+        } else if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O_MR1){
+            getWindow().setNavigationBarColor(Color.BLACK);
+        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commit();
+    }
+}
---
+++ app/src/main/java/com/f0x1d/cripty/fragment/MainFragment.java
@@ -0,0 +1,255 @@
+package com.f0x1d.cripty.fragment;
+
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import com.f0x1d.cripty.R;
+import com.f0x1d.cripty.activity.MainActivity;
+import com.google.android.material.button.MaterialButton;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+public class MainFragment extends Fragment {
+
+    public static MainFragment newInstance() {
+        Bundle args = new Bundle();
+
+        MainFragment fragment = new MainFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public MainActivity getMainActivity(){
+        return (MainActivity) getActivity();
+    }
+
+    public MaterialButton encrypt;
+    public MaterialButton decrypt;
+
+    public final int ENCRYPT_CODE = 0;
+    public final int DECRYPT_CODE = 1;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.main_fragment, container, false);
+
+        encrypt = v.findViewById(R.id.encrypt);
+        decrypt = v.findViewById(R.id.decrypt);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.decrypt)
+                    openFile("*/*", DECRYPT_CODE, getActivity());
+                else if (v.getId() == R.id.encrypt)
+                    openFile("*/*", ENCRYPT_CODE, getActivity());
+            }
+        };
+
+        encrypt.setOnClickListener(listener);
+        decrypt.setOnClickListener(listener);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityResult(final int requestCode, int resultCode, @Nullable final Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (data == null)
+            return;
+
+        View v = LayoutInflater.from(getMainActivity()).inflate(R.layout.edit_text, null);
+        final EditText editText = v.findViewById(R.id.edittext);
+        editText.setHint("AES Key");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getMainActivity());
+        builder.setTitle("Input your AES key");
+        builder.setView(v);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                final ProgressDialog progressDialog = new ProgressDialog(getMainActivity());
+                progressDialog.setMessage("Processing...");
+                progressDialog.setCancelable(false);
+                progressDialog.show();
+
+                String key = editText.getText().toString();
+
+                try {
+                    File appFolder = new File(Environment.getExternalStorageDirectory() + "/Crypty");
+                    if (!appFolder.exists())
+                        appFolder.mkdirs();
+
+                    InputStream realInputStream = getMainActivity().getContentResolver().openInputStream(data.getData());
+
+                    File realFile = null;
+
+                    try {
+                        realFile = new File(getMainActivity().getCacheDir(), "cache");
+                        OutputStream output = new FileOutputStream(realFile);
+                        try {
+                            byte[] buffer = new byte[4 * 1024];
+                            int read;
+
+                            while ((read = realInputStream.read(buffer)) != -1) {
+                                output.write(buffer, 0, read);
+                            }
+
+                            output.flush();
+                        } finally {
+                            output.close();
+                        }
+                    } finally {
+                        realInputStream.close();
+                    }
+
+                    File cryptedFile = null;
+
+                    if (requestCode == ENCRYPT_CODE)
+                        cryptedFile = new File(appFolder, "encrypted_" + getFileName(data.getData()) + "." + getMimeType(getActivity(), data.getData()));
+                    else if (requestCode == DECRYPT_CODE)
+                        cryptedFile = new File(appFolder, "decrypted_" + getFileName(data.getData()) + "." + getMimeType(getActivity(), data.getData()));
+
+                    SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(), "AES");
+                    Cipher cipher = Cipher.getInstance("AES");
+                    if (requestCode == ENCRYPT_CODE)
+                        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+                    else if (requestCode == DECRYPT_CODE)
+                        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+
+                    FileInputStream inputStream = new FileInputStream(realFile);
+                    byte[] inputBytes = new byte[(int) realFile.length()];
+                    inputStream.read(inputBytes);
+
+                    byte[] outputBytes = cipher.doFinal(inputBytes);
+
+                    FileOutputStream outputStream = new FileOutputStream(cryptedFile);
+                    outputStream.write(outputBytes);
+
+                    inputStream.close();
+                    outputStream.close();
+
+                    progressDialog.cancel();
+                    Toast.makeText(getActivity(), "Successfully saved to " + cryptedFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                } catch (Exception e){
+                    progressDialog.cancel();
+                    processError(e);
+                }
+            }
+        });
+        builder.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+    }
+
+    public void processError(Exception e){
+        AlertDialog.Builder builder = new AlertDialog.Builder(getMainActivity());
+        builder.setTitle("Error!");
+        builder.setMessage(e.getLocalizedMessage());
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.show();
+
+        e.printStackTrace();
+    }
+
+    public String getFileName(Uri uri) {
+        String result = null;
+        if (uri.getScheme().equals("content")) {
+            Cursor cursor = getContext().getContentResolver().query(uri, null, null, null, null);
+            try {
+                if (cursor != null && cursor.moveToFirst()) {
+                    result = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+        if (result == null) {
+            result = uri.getPath();
+            int cut = result.lastIndexOf('/');
+            if (cut != -1) {
+                result = result.substring(cut + 1);
+            }
+        }
+        return result;
+    }
+
+    public static String getMimeType(Context context, Uri uri) {
+        String extension;
+
+        if (uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            final MimeTypeMap mime = MimeTypeMap.getSingleton();
+            extension = mime.getExtensionFromMimeType(context.getContentResolver().getType(uri));
+        } else {
+            extension = MimeTypeMap.getFileExtensionFromUrl(Uri.fromFile(new File(uri.getPath())).toString());
+
+        }
+
+        return extension;
+    }
+
+    public void openFile(String minmeType, int requestCode, Context c) {
+
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType(minmeType);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // special intent for Samsung file manager
+        Intent sIntent = new Intent("com.f0x1d.notes.main.PICK_DATA");
+        // if you want any file type, you can skip next line
+        sIntent.putExtra("CONTENT_TYPE", minmeType);
+        sIntent.addCategory(Intent.CATEGORY_DEFAULT);
+
+        Intent chooserIntent;
+        if (c.getPackageManager().resolveActivity(sIntent, 0) != null) {
+            // it is device with samsung file manager
+            chooserIntent = Intent.createChooser(sIntent, "Open file");
+            chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[]{intent});
+        } else {
+            chooserIntent = Intent.createChooser(intent, "Open file");
+        }
+
+        try {
+            startActivityForResult(chooserIntent, requestCode);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(getActivity(), "No suitable File Manager was found.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
